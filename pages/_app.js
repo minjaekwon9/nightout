9253b80eac92b09e361353091bf19c54a9ceb09c
Minjae Kwon <minjaekwon9@gmail.com>
2022-10-11 04:25:59 -0700

    Added email form to Contact page with pop up notifs

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,9 @@
+// LIBRARY IMPORTS
 import "bootstrap/dist/css/bootstrap.min.css"
+import "notiflix/dist/notiflix-3.2.5.min.css"
+import "notiflix/dist/notiflix-3.2.5.min.js"
+
+// CUSTOM IMPORTS
 import "../styles/globals.css"
 
 function MyApp({ Component, pageProps }) {
--- pages/contact.js
+++ pages/contact.js
@@ -1,11 +1,84 @@
 // LIBRARY IMPORTS
+import React, { useRef } from 'react'
+import Button from 'react-bootstrap/Button'
+import Form from 'react-bootstrap/Form'
+import Container from 'react-bootstrap/Container'
+import Col from 'react-bootstrap/Col'
+import Row from 'react-bootstrap/Row'
+import emailjs from '@emailjs/browser'
+import Notiflix from 'notiflix'
+
+// CUSTOM IMPORTS
 import { default as Navbar } from '../components/Navbar'
 
 export default function Contact() {
+
+    const form = useRef()
+
+    const sendEmail = (e) => {
+        e.preventDefault()
+        emailjs.sendForm('contact_service', 'contact_form', form.current, '3N2FH8Rf_RLs67D4S')
+            .then(() => {
+                Notiflix.Notify.success("Your message has been sent, we will get back to you shortly!",
+                    { timeout: 10000, fontSize: "1rem", width: "530px", position: "center-top", clickToClose: true })
+            }, (error) => {
+                Notiflix.Notify.failure("An error has occurred, please try again.",
+                    { timeout: 10000, fontSize: "1rem", width: "380px", position: "center-top", clickToClose: true })
+            })
+        e.target.reset()
+    }
+
     return (
         <div>
             <Navbar />
-            This is the Contact Us page.
+            {/* Sends an email to minjaekwon24601@gmail.com using EmailJS */}
+            <Container
+                className='my-5 p-4 rounded'
+                style={{ backgroundColor: "#556d7c", maxWidth: 600 }}
+            >
+                <h1 className='mb-4 text-center'>Shoot us an email!</h1>
+                <Form ref={form} onSubmit={sendEmail}>
+                    <Form.Group as={Row} className="mb-3" controlId="contactFormName">
+                        <Form.Label column sm="2">Name: </Form.Label>
+                        <Col sm="10">
+                            <Form.Control
+                                required
+                                name="user_name"
+                                type="text"
+                                placeholder="Enter your name"
+                            />
+                        </Col>
+                    </Form.Group>
+                    <Form.Group as={Row} className="mb-3" controlId="contactFormEmail">
+                        <Form.Label column sm="2">Email: </Form.Label>
+                        <Col sm="10">
+                            <Form.Control
+                                required
+                                name="user_email"
+                                type="email"
+                                placeholder="Enter your email"
+                            />
+                        </Col>
+                    </Form.Group>
+                    <Form.Group as={Row} className="mb-3" controlId="contactFormMsg">
+                        <Form.Label column sm="2">Message: </Form.Label>
+                        <Col sm="10">
+                            <Form.Control
+                                required
+                                name="message"
+                                as="textarea"
+                                rows={3}
+                                placeholder="Enter your message"
+                            />
+                        </Col>
+                    </Form.Group>
+                    <div className='text-center'>
+                        <Button variant="outline-light" type="submit">
+                            Submit
+                        </Button>
+                    </div>
+                </Form>
+            </Container>
         </div>
     )
 }
